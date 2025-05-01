6dc046ee0d6dcf826ca00b26019c9c08c444296d
kazuki005 <kakazuki_yakyu@icloud.com>
2025-05-01 17:49:26 +0900

    modify tech_catch/urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.http import JsonResponse
-
+from tech_catch.views import index
 def index_view(request):
     return JsonResponse({"message": "TechCatch backend is alive."})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view),
     path('api/', include('tech_catch.urls')),  
+    re_path(r'^.*$', index),
 ]
